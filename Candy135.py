2b8db07f4af1bcd3da7701cd187685f8af94c54a
Duncan-Genge <102703124+D-Genge@users.noreply.github.com>
2024-04-05 21:35:41 -0400

    135

---
+++ Candy135.py
@@ -0,0 +1,16 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        if not ratings:
+            return 0
+        if len(ratings) == 1:
+            return 1
+        
+        candy = [1] * len(ratings)
+        for i in range(1, len(ratings)): # left to right pass
+            if ratings[i] > ratings[i-1]:
+                candy[i] = candy[i-1] + 1
+        for i in range(len(ratings) - 2, -1, -1):
+            if ratings[i] > ratings[i+1]:
+                candy[i] = max(candy[i], candy[i+1] + 1)
+        return sum(candy)
+                
